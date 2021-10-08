83f0006e50a8b06967dc97bcd9f41076526bb224
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-10-08 13:02:49 +0700

    inline css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Transaction from './Component/Transaction'
 function App() {
   return (
     <>
-      <h1>โปรแกรมบัญชีรายรับ - รายจ่าย</h1>
+      <h1 style={{color:'red',textAlign:'center',fontSize:'1.5rem'}}>โปรแกรมบัญชีรายรับ - รายจ่าย</h1>
       <Transaction></Transaction>
     </>
   );
