46d57d0bef913bb12ceab3fe47b7d04ad4790bdd
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-10-08 12:43:51 +0700

    nested component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+const Title = () => <h1>โปรแกรมบัญชีรายรับ - รายจ่าย</h1>;
+const Describtion = () => <p>บันทึกข้อมูลบัญชีในแต่ละวัน</p>;
+const Transaction = () => (
+  <ul>
+    <li>
+      ค่าเดินทาง <span>-200</span>
+    </li>
+    <li>
+      เงินเดือน <span>+20,000</span>
+    </li>
+    <li>
+      ค่าอาหาร <span>-500</span>
+    </li>
+  </ul>
+);
+
 function App() {
   return (
     <>
-      <article>
-        <h1>โปรแกรมบัญชีรายรับ - รายจ่าย</h1>
-        <p>บันทึกข้อมูลบัญชีในแต่ละวัน</p>
-        <ul>
-          <li>
-            ค่าเดินทาง <span>-200</span>
-          </li>
-          <li>
-            เงินเดือน <span>+20,000</span>
-          </li>
-          <li>
-            ค่าอาหาร <span>-500</span>
-          </li>
-        </ul>
-      </article>
+      <Title></Title>
+      <Describtion></Describtion>
+      <Transaction></Transaction>
     </>
   );
 }
