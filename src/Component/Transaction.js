cfd2e97a9b136607684c2094002bf7eaea7e74be
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-10-14 16:44:53 +0700

    key

--- src/Component/Transaction.js
+++ src/Component/Transaction.js
@@ -3,13 +3,13 @@ import "./Transaction.css";
 
 const Transaction = () => {
   const data = [
-    {title:'ค่ารักษาพยาบาล',amount:2000},
-    {title:'ค่าน้ำมัน',amount:5000},
-    {title:'ค่าเช่าบ้าน',amount:8000},
+    {id:1,title:'ค่ารักษาพยาบาล',amount:2000},
+    {id:2,title:'ค่าน้ำมัน',amount:5000},
+    {id:3,title:'ค่าเช่าบ้าน',amount:8000},
   ]
   return (
     <ul className="item-list">
-      {data.map((element)=><Item {...element}></Item>)}
+      {data.map((element)=><Item {...element} key={element.id}></Item>)}
     </ul>
   );
 };
