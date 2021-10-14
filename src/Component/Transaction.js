570cc650e34fc2c3643b85e32f2c1809c290361d
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-10-14 16:49:45 +0700

    generate uuid

--- src/Component/Transaction.js
+++ src/Component/Transaction.js
@@ -1,15 +1,16 @@
 import Item from "./Item";
 import "./Transaction.css";
+import {v4 as uuidv4} from 'uuid';
 
 const Transaction = () => {
   const data = [
-    {id:1,title:'ค่ารักษาพยาบาล',amount:2000},
-    {id:2,title:'ค่าน้ำมัน',amount:5000},
-    {id:3,title:'ค่าเช่าบ้าน',amount:8000},
+    {title:'ค่ารักษาพยาบาล',amount:2000},
+    {title:'ค่าน้ำมัน',amount:5000},
+    {title:'ค่าเช่าบ้าน',amount:8000},
   ]
   return (
     <ul className="item-list">
-      {data.map((element)=><Item {...element} key={element.id}></Item>)}
+      {data.map((element)=><Item {...element} key={uuidv4()}></Item>)}
     </ul>
   );
 };
