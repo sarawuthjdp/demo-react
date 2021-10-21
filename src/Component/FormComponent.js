7062c7cc4dc09ea495f92ef0b755c554b232fb3b
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-10-21 19:22:52 +0700

    event

--- src/Component/FormComponent.js
+++ src/Component/FormComponent.js
@@ -1,16 +1,26 @@
 import './FormComponent.css'
 
 const FormComponent = ()=>{
+    const inputTitle = (event)=>{
+        console.log(event.target.value);
+    }
+    const inputAmount = (event)=>{
+        console.log(event.target.value);
+    }
+    const saveItem = (event)=>{
+        event.preventDefault();
+        console.log("บันทึกข้อมูลเรียบร้อย");
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={saveItem}>
                 <div className="form-control">
                     <label>ชื่อรายการ</label>
-                    <input type="text" placeholder="ระบุชื่อรายการของคุณ" />
+                    <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle}/>
                 </div>
                 <div className="form-control">
                     <label>จำนวนเงิน</label>
-                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)" />
+                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)" onChange={inputAmount}/>
                 </div>
                 <div className="form-control">
                     <button type="submit" className="btn">เพิ่มข้อมูล</button>
