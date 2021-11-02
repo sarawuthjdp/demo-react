6f187b59a6e0a3ef1002b6338951a16321f4e6da
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-11-02 16:42:49 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import { useEffect, useReducer, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Transaction from "./Component/Transaction";
 import FormComponent from "./Component/FormComponent";
 import './App.css'
 import DataContext from './data/DataContext';
 import ReportComponent from './Component/ReportComponent';
+import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 function App() {
   const design = { color: "red", textAlign: "center", fontSize: "1.5rem" };
   const onAddNewItem = (newItems)=>{
     setItems([newItems,...items]);
   }
 
-  const [items, setItems] = useState([])
+  const initData = [
+    {id:1,title:'ค่าเช่าบ้าน',amount:-3000},
+    {id:2,title:'เงินเดือน',amount:50000}
+  ]
+
+  const [items, setItems] = useState(initData)
 
   const [reportIncome,setReportIncome] = useState(0)
   const [reportExpense,setReportExpense] = useState(0)
   useEffect(()=>{
-    setReportIncome(items.map(items=>items.amount).filter(item=>item>0).reduce((total,curr)=>total+=curr,0))
-    setReportExpense(items.map(items=>items.amount).filter(item=>item<0).reduce((total,curr)=>total+=curr,0)*-1)
+    setReportIncome(items.map(items=>items.amount).filter(item=>item>0).reduce((total,curr)=>total+=curr,0).toFixed(2))
+    setReportExpense((items.map(items=>items.amount).filter(item=>item<0).reduce((total,curr)=>total+=curr,0)*-1).toFixed(2))
   },[items]);
 
-  const [showReport,setShowReport] = useState(false)
-  const reducer = (state,action)=>{
-    switch(action.type){
-      case 'SHOW':
-        return setShowReport(true);
-      case 'HIDE':
-        return setShowReport(false);
-    }
-  }
-  const [result,dispatch] = useReducer(reducer,showReport)
   return (
     <DataContext.Provider value={{income:reportIncome,expense:reportExpense}}>
       <div div className='container'>
         <h1 style={design}>แอพบัญชีรายรับ - รายจ่าย</h1>
-        {showReport && <ReportComponent/>}
-        <FormComponent onAddItem={onAddNewItem}></FormComponent>
-        <Transaction items={items}></Transaction>
-        <button onClick={()=>dispatch({type:'SHOW'})}>แสดง</button>
-        <button onClick={()=>dispatch({type:'HIDE'})}>ซ่อน</button>
+        <Router>
+          <div>
+            <ul className='horizontal-menu'>
+              <li>
+                <Link to='/'>ข้อมูลบัญชี</Link> 
+              </li>
+              <li>
+                <Link to='/insert'>บันทึกข้อมูล</Link>
+              </li>
+            </ul>
+            <Switch>
+              <Route path='/' exact>
+                <ReportComponent/>
+              </Route>
+              <Route patch='/insert'>
+                <FormComponent onAddItem={onAddNewItem}></FormComponent>
+                <Transaction items={items}></Transaction>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     </DataContext.Provider> 
   );
