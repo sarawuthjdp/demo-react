934f7612126c804dc85012ee4c992d0502c61160
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-11-02 16:13:29 +0700

    show&hide using reducer

--- src/App.js
+++ src/App.js
@@ -9,13 +9,8 @@ function App() {
   const onAddNewItem = (newItems)=>{
     setItems([newItems,...items]);
   }
-  const initState = [
-    {id:1,title:"ค่าเช่าบ้าน",amount:-2000},
-    {id:2,title:"เงินเดือน",amount:20000},
-    {id:3,title:"ค่าเดินทาง",amount:-500},
-    {id:4,title:"ขายของ",amount:2000}
-  ]
-  const [items, setItems] = useState(initState)
+
+  const [items, setItems] = useState([])
 
   const [reportIncome,setReportIncome] = useState(0)
   const [reportExpense,setReportExpense] = useState(0)
@@ -24,34 +19,27 @@ function App() {
     setReportExpense(items.map(items=>items.amount).filter(item=>item<0).reduce((total,curr)=>total+=curr,0)*-1)
   },[items]);
 
-  const [count,setCount] = useState(0)
+  const [showReport,setShowReport] = useState(false)
   const reducer = (state,action)=>{
-    console.log(action)
     switch(action.type){
-      case 'ADD':
-        return state+action.payload;
-      case 'SUB':
-        return state-action.payload;
-      case 'CLEAR':
-        return 0;
+      case 'SHOW':
+        return setShowReport(true);
+      case 'HIDE':
+        return setShowReport(false);
     }
   }
-  const [result,dispatch] = useReducer(reducer,count)
+  const [result,dispatch] = useReducer(reducer,showReport)
   return (
-    // <DataContext.Provider value={{income:reportIncome,expense:reportExpense}}>
-    //   <div div className='container'>
-    //     <h1 style={design}>แอพบัญชีรายรับ - รายจ่าย</h1>
-    //     <ReportComponent/>
-    //     <FormComponent onAddItem={onAddNewItem}></FormComponent>
-    //     <Transaction items={items}></Transaction>
-    //   </div>
-    // </DataContext.Provider> 
-    <div align="center">
-      <h1>{result}</h1>
-      <button onClick={()=>dispatch({type:'ADD',payload:10})}>เพิ่ม</button>
-      <button onClick={()=>dispatch({type:'SUB',payload:5})}>ลด</button>
-      <button onClick={()=>dispatch({type:'CLEAR'})}>ล้าง</button>
-    </div>
+    <DataContext.Provider value={{income:reportIncome,expense:reportExpense}}>
+      <div div className='container'>
+        <h1 style={design}>แอพบัญชีรายรับ - รายจ่าย</h1>
+        {showReport && <ReportComponent/>}
+        <FormComponent onAddItem={onAddNewItem}></FormComponent>
+        <Transaction items={items}></Transaction>
+        <button onClick={()=>dispatch({type:'SHOW'})}>แสดง</button>
+        <button onClick={()=>dispatch({type:'HIDE'})}>ซ่อน</button>
+      </div>
+    </DataContext.Provider> 
   );
 }
 
